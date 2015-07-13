1123ba10bd41826f5dbc9c66deeea0878d1e2aac
tunnyios <shc245245@126.com>
2015-07-13 15:42:30 +0800

    创建A-cyclist项目

---
+++ A-cyclist/A-cyclist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A-cyclist
+//
+//  Created by tunny on 15/7/13.
+//  Copyright (c) 2015年 tunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ A-cyclist/A-cyclist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A-cyclist
+//
+//  Created by tunny on 15/7/13.
+//  Copyright (c) 2015年 tunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
