afc2ae1ba5c94100c69b93ef494368bb34936665
tunnyios <shc245245@126.com>
2015-07-24 16:01:18 +0800

    修改完成骑行界面的UI搭建 使用scrollView来实现切换两个view

---
+++ A-cyclist/A-cyclist/Class/Cycling/View/ACCyclingViewController.h
@@ -0,0 +1,13 @@
+//
+//  ACCyclingViewController.h
+//  A-cyclist
+//
+//  Created by tunny on 15/7/24.
+//  Copyright (c) 2015年 tunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACCyclingViewController : UIViewController
+
+@end
